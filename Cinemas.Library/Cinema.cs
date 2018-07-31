60edc057652ec4a54277333a70cdfaf7cef839c6
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-30 21:00:39 -0400

    Added movies and initial menu

---
+++ Cinemas.Library/Cinema.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemas.Library
+{
+    public class Cinema
+    {
+        public string Movie { get; set; }
+        public string Genre { get; set; }
+
+        public Cinema(string movie, string genre)
+        {
+            Movie = movie;
+            Genre = genre;
+        }
+
+    }
+    
+}
--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cinemas.Library;
 
 namespace Lab11
 {
@@ -10,6 +11,22 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
+            List<Cinema> movieList = new List<Cinema> { };
+            movieList.Add(new Cinema("The Bee Movie", "Comedy"));
+            movieList.Add(new Cinema("PeeWee's Great Adventure", "Comedy"));
+            movieList.Add(new Cinema("Hot Rod", "Comedy"));
+            movieList.Add(new Cinema("Lord of the Rings", "Fantasy"));
+            movieList.Add(new Cinema("The Thief of Bagdad", "Fantasy"));
+            movieList.Add(new Cinema("The Princess Bride", "Fantasy"));
+            movieList.Add(new Cinema("50 First Dates", "Rom-Com"));
+            movieList.Add(new Cinema("The Notebook", "Rom-Com"));
+            movieList.Add(new Cinema("Friday the 13th", "Horror"));
+            movieList.Add(new Cinema("The Strangers", "Horror"));
+
+            Console.WriteLine("Which type of movie do you want to see?\n1.\tComedy\n2.\tFantasy\n3.\tRom-Com\n4.\tHorror");
+            Console.ReadLine();
+
+
         }
     }
 }
